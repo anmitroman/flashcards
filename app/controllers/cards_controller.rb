985684078a79aa2a8229d33b0e6ebf242bf4726e
anmitroman <an.mankovskaya@yandex.ru>
2019-11-02 00:13:36 +0300

    again hound's recommendations done

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 # ApplicationController inherits from ActionController::Base, which defines a number of helpful methods
+
 class CardsController < ApplicationController
   def index
     @card = Card.all
