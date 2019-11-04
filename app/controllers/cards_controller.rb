e73bf258abea8c7b6189d766844669500486a0b1
anmitroman <an.mankovskaya@yandex.ru>
2019-11-04 08:22:40 +0300

    added file hound.yml

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,6 +1,3 @@
-# frozen_string_literal: true
-# ApplicationController inherits from ActionController::Base, which defines a number of helpful methods
-
 class CardsController < ApplicationController
   def index
     @card = Card.all
