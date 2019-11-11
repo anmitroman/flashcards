01a358f0688e266e9c066edbbac3a38e0b549382
anmitroman <46959111+anmitroman@users.noreply.github.com>
2019-11-11 17:20:07 +0300

    trailing whitespace removed

--- app/models/card.rb
+++ app/models/card.rb
@@ -3,7 +3,7 @@ class Card < ApplicationRecord
   validate :original_text_equal_translated_text
  
   before_create :add_date
-   
+  
   def original_text_equal_translated_text
     if original_text.downcase == translated_text.downcase
       errors.add(:original_text, "can't be equal translated_text")
@@ -11,6 +11,7 @@ class Card < ApplicationRecord
   end
 
   private
+
   def add_date
     self.review_date = Date.today + 3
   end
