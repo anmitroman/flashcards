67153401d50570373d91e9dd853ae3f95508aa13
anmitroman <an.mankovskaya@yandex.ru>
2019-11-13 22:32:46 +0300

    last commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 puts '### Search for nodes by xpath'
 data = Parser.parse_en365
+puts data
 data.each do |word|
   puts "#{word[0]} - added to database"
   Card.create(original_text: word[0], translated_text: word[1], review_date: word[2])
