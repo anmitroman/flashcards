9a9cb53a91648376905e0f0edd351ab0e4e0718b
anmitroman <an.mankovskaya@yandex.ru>
2019-11-22 23:34:10 +0300

    added context

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,35 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Card, type: :model do
-  before(:all) do
-    @card = create(:card)
-  end
+  let!(:card1) { create(:card) }
 
-  it 'has a original text' do
-    expect(@card.original_text).to eq('sun')
-  end
+  context 'when creating or updating card' do
+    it 'has a original text' do
+      expect(card1.original_text).to eq('sun')
+    end
 
-  it 'has not equivalent texts' do
-    card2 = build(:card, translated_text: 'sun')
-    expect(card2).to_not be_valid
-  end
+    it 'has not equivalent texts' do
+      card2 = build(:card, translated_text: 'sun')
+      expect(card2).to_not be_valid
+    end
 
-  it 'is not valid without translated_text' do
-    card2 = build(:card, translated_text: '')
-    expect(card2).to_not be_valid
-  end
+    it 'is not valid without translated_text' do
+      card2 = build(:card, translated_text: '')
+      expect(card2).to_not be_valid
+    end
 
-  it 'has a unique original text' do
-    card2 = build(:card, original_text: 'sun')
-    expect(card2).to_not be_valid
-  end
+    it 'has a unique original text' do
+      card2 = build(:card, original_text: 'sun')
+      expect(card2).to_not be_valid
+    end
 
-  it 'is not valid without original_text' do
-    card2 = build(:card, original_text: '')
-    expect(card2).to_not be_valid
-  end
+    it 'is not valid without original_text' do
+      card2 = build(:card, original_text: '')
+      expect(card2).to_not be_valid
+    end
 
-  it 'is nas review_date = today + 3' do
-    expect(@card.review_date).to eq(Date.today + 3)
+    it 'has review_date = today + 3' do
+      expect(card1.review_date).to eq(Date.today + 3)
+    end
   end
 end
