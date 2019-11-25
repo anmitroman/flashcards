88fa9b455c513f41753fa3b80fe27c8b6d74cad9
anmitroman <an.mankovskaya@yandex.ru>
2019-11-25 21:24:51 +0300

    improved specs

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -3,33 +3,44 @@ require 'rails_helper'
 RSpec.describe Card, type: :model do
   let!(:card1) { create(:card) }
 
-  context 'when creating or updating card' do
-    it 'has a original text' do
+  context 'when card has original text' do
+    it 'shoud be "sun"' do
       expect(card1.original_text).to eq('sun')
     end
+  end
 
-    it 'has not equivalent texts' do
+  context 'when translated text is equal original text' do
+    it 'should not be valid' do
       card2 = build(:card, translated_text: 'sun')
       expect(card2).to_not be_valid
     end
+  end
 
-    it 'is not valid without translated_text' do
+  context 'when translated text is empty' do
+    it 'should not be valid' do
       card2 = build(:card, translated_text: '')
       expect(card2).to_not be_valid
     end
+  end
 
-    it 'has a unique original text' do
-      card2 = build(:card, original_text: 'sun')
+  context 'when creating the second identical card' do
+    it 'should not be valid' do
+      card2 = build(:card, original_text: 'sun', translated_text: 'солнце')
       expect(card2).to_not be_valid
     end
+  end
 
-    it 'is not valid without original_text' do
+  context 'when original text is empty' do
+    it 'should not be valid' do
       card2 = build(:card, original_text: '')
       expect(card2).to_not be_valid
     end
+  end
 
-    it 'has review_date = today + 3' do
+  context 'when creating new card' do
+    it 'must have review_date = today + 3' do
       expect(card1.review_date).to eq(Date.today + 3)
     end
   end
+  
 end
